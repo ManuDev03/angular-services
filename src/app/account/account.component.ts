041c4f704d134a92af2b646004c82889da87d23a
ManuDev03 <manuprajan03@gmail.com>
2021-08-12 12:16:29 +0530

    Injecting services into Services

--- src/app/account/account.component.ts
+++ src/app/account/account.component.ts
@@ -5,7 +5,7 @@ import { LoggingService } from '../logging.service';
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css'],
-  providers:[LoggingService]
+  // providers:[LoggingService]
 })
 export class AccountComponent implements OnInit {
 
@@ -19,7 +19,7 @@ export class AccountComponent implements OnInit {
 
   onSetTo(status: string) {
     this.accountsService.updateStatus(this.id,status)
-    this.loggingService.logStatusChange(status)
+    // this.loggingService.logStatusChange(status)
   }
 
 
--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -6,7 +6,7 @@ import { LoggingService } from '../logging.service';
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers:[LoggingService]
+  // providers:[LoggingService]
 })
 export class NewAccountComponent implements OnInit {
 
@@ -16,7 +16,7 @@ export class NewAccountComponent implements OnInit {
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountsService.addAccount(accountName,accountStatus)
-    this.loggingService.logStatusChange(accountStatus)
+    // this.loggingService.logStatusChange(accountStatus)
  
   }
 
